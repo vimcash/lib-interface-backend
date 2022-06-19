82702c265cce4eac6239459fc2f8952e0124d599
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-19 15:37:39 -0400

    create library

---
+++ src/SimpleUser/SimpleUser.ts
@@ -0,0 +1,12 @@
+/**
+ * This is a simple user interface
+ * @param username String
+ * @param password String
+ */
+
+interface SimpleUser {
+    username: String;
+    password: String;
+}
+
+export default SimpleUser;
