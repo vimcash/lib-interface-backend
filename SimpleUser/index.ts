e675f5d9e6fa046c7dac53a46735b8f25bef0868
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-20 13:47:33 -0400

    Modificacion

--- SimpleUser/index.ts
+++ SimpleUser/index.ts
@@ -1,2 +1,12 @@
-export { default } from "./SimpleUser";
-export { SimpleUser } from "./SimpleUser";
+/**
+ * This is a simple user interface
+ * @param username String
+ * @param password String
+ */
+
+ export interface SimpleUser {
+    username: String;
+    password: String;
+}
+
+export default SimpleUser;
