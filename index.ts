5ba7db9fb0e471cf583d17f66b7ae68c5b105578
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-26 23:12:55 -0400

    Arreglos

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
+// A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+
 export { BBTurnDevice } from "./BBTurnDevice";
-export { BBTurnDeviceTable } from "./BBTurnDevice/BBTurnDeviceTable";
 export { Device } from "./Device";
 export { DeviceStatus } from "./Core/Dependencies/Enums/DeviceStatus";
-export { DeviceTable } from "./Device/DeviceTable";
 export { Product } from "./Product";
-export { ProductTable } from "./Product/ProductTable";
 export { SimpleCompany } from "./SimpleCompany";
-export { SimpleCompanyTable } from "./SimpleCompany/SimpleCompanyTable";
 export { SimpleUser } from "./SimpleUser";
-export { SimpleUserTable } from "./SimpleUser/SimpleUserTable";
 export { TradicionalCompany } from "./TradicionalCompany";
-export { TradicionalCompanyTable } from "./TradicionalCompany/TradicionalCompanyTable";
 export { TradicionalUser } from "./TradicionalUser";
+
+export { BBTurnDeviceTable } from "./BBTurnDevice/BBTurnDeviceTable";
+export { DeviceTable } from "./Device/DeviceTable";
+export { ProductTable } from "./Product/ProductTable";
+export { SimpleCompanyTable } from "./SimpleCompany/SimpleCompanyTable";
+export { SimpleUserTable } from "./SimpleUser/SimpleUserTable";
+export { TradicionalCompanyTable } from "./TradicionalCompany/TradicionalCompanyTable";
 export { TradicionalUserTable } from "./TradicionalUser/TradicionalUserTable";
