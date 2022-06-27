35c2ea27c10d8dfd1a051aa1fce9bc8bb649d9ff
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-27 00:09:24 -0400

    limpieza

--- TradicionalUser/TradicionalUserTable/index.d.ts
+++
@@ -1,8 +0,0 @@
-import TradicionalUser from "..";
-import { DataControl } from "../../Core/Dependencies/DataContol";
-/**
- * This is a simple User interface with Table Paramameters
- */
-export interface TradicionalUserTable extends TradicionalUser, DataControl {
-}
-export default TradicionalUserTable;
