28fa4f93ef4470209e6c0dcf2242dac90c40a3fa
Raian M. Gil del Rosario <Raian.gdr@gmail.com>
2022-06-28 22:17:45 -0400

    fix: Agregando CompanyUser al index

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
 
 export { BBTurnDevice } from "./BBTurnDevice";
+export { CompanyUser } from "./CompanyUser"
 export { Device } from "./Device";
 export { DeviceStatus } from "./Core/Dependencies/Enums/DeviceStatus";
 export { Inventory } from "./Inventory";
@@ -11,6 +12,7 @@ export { TraditionalCompany } from "./TraditionalCompany";
 export { TraditionalUser } from "./TraditionalUser";
 
 export { BBTurnDeviceTable } from "./BBTurnDevice/BBTurnDeviceTable";
+export { CompanyUserTable } from "./CompanyUser/CompanyUserTable"
 export { DeviceTable } from "./Device/DeviceTable";
 export { InventoryTable } from "./Inventory/InventoryTable";
 export { ProductTable } from "./Product/ProductTable";
